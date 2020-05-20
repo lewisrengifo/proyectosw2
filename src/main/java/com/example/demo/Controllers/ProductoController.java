7524022c5a37d6609a1a1be89f51f5ebf251ce87
Gabrielcv98 <gabrialbert98@gmail.com>
2020-05-19 21:02:03 -0500

    Producto con paginacion (cosas por mejorar)

--- src/main/java/com/example/demo/Controllers/ProductoController.java
+++ src/main/java/com/example/demo/Controllers/ProductoController.java
@@ -1,24 +1,54 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Dto.ProductoServiceApi;
 import com.example.demo.Entity.Producto;
 import com.example.demo.Repository.ProductoRepository;
+import com.example.demo.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/producto")
 public class ProductoController {
     @Autowired
+    ProductoServiceApi productoServiceApi;
+
+    @Autowired
     ProductoRepository productoRepository;
 
     @GetMapping(value = {"", "/"})
-    public String listaProductos(Model model) {
-        model.addAttribute("listaProductos", productoRepository.findAll());
+    public String listaProduct(@RequestParam Map<String,Object> params,Model model) {
+
+        int page = params.get("page") != null ? (Integer.valueOf(params.get("page").toString()) - 1) : 0;
+
+        PageRequest pageRequest = PageRequest.of(page,10);
+
+        Page<Producto> pageProduct = productoServiceApi.getAll(pageRequest);
+
+        int totalPage = pageProduct.getTotalPages();
+        if(totalPage > 0 ){
+            List<Integer> pages = IntStream.rangeClosed(1,totalPage).boxed().collect(Collectors.toList());
+            model.addAttribute("pages",pages);
+        }
+
+        model.addAttribute("listaProductos", pageProduct.getContent());
+        model.addAttribute("current",page + 1);
+        model.addAttribute("next",page + 2);
+        model.addAttribute("prev",page);
+        model.addAttribute("last",totalPage);
+
         return "producto/listar";
     }
     @GetMapping("/nuevo")
@@ -63,4 +93,7 @@ public class ProductoController {
         return "redirect:/producto";
 
     }
+
+
+
 }
