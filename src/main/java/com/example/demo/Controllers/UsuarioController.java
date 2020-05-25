b108b9756a1f69eb18631373e8694f75974c1ee9
lewis-coder <57689615+lewis-coder@users.noreply.github.com>
2020-05-24 23:49:28 -0500

    crud usuario completo sin paginacion

--- src/main/java/com/example/demo/Controllers/UsuarioController.java
+++ src/main/java/com/example/demo/Controllers/UsuarioController.java
@@ -5,11 +5,15 @@ import com.example.demo.Repository.RolRepository;
 import com.example.demo.Repository.SedeRepository;
 import com.example.demo.Repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.naming.Binding;
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -21,39 +25,68 @@ public class UsuarioController {
     RolRepository rolRepository;
     @Autowired
     SedeRepository sedeRepository;
+
     @GetMapping("/lista")
-    public String listarUsuarios(Model model){
+    public String listarUsuarios(Model model) {
         model.addAttribute("listaUsuarios", usuarioRepository.findAll());
         return "Usuario/lista";
     }
+
     @GetMapping("/nuevo")
-    public String nuevoUsuario(Model model, @ModelAttribute Usuario usuario){
+    public String nuevoUsuario(Model model, @ModelAttribute Usuario usuario) {
         model.addAttribute("listaroles", rolRepository.findAll());
         model.addAttribute("listasedes", sedeRepository.findAll());
         return "Usuario/form";
     }
+
     @PostMapping("/guardar")
-    public String guardar(@ModelAttribute("usuario") Usuario usuario, RedirectAttributes redirectAttributes ){
-        if (usuario.getIdusuario() == 0) {
-            redirectAttributes.addFlashAttribute("msg", "Usuario creado exitosamente");
-        } else {
-            redirectAttributes.addFlashAttribute("msg", "Usuario actualizado exitosamente");
+    public String guardar(@ModelAttribute("usuario") @Valid Usuario usuario, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("listaroles", rolRepository.findAll());
+            model.addAttribute("listasedes", sedeRepository.findAll());
+            return "Usuario/form";
         }
+
+        for (Usuario usuario1 : usuarioRepository.findAll()) {
+            if (usuario1.getDni().equals(usuario.getDni()) || usuario1.getCorreo().equals(usuario.getCorreo())) {
+                if (usuario1.getDni().equals(usuario.getDni())) {
+                    redirectAttributes.addFlashAttribute("msg", "Usuario con DNI existente");
+                    redirectAttributes.addFlashAttribute("usuario", usuario);
+                } if(usuario1.getCorreo().equals(usuario.getCorreo())) {
+                    redirectAttributes.addFlashAttribute("msg2", "Usuario con correo existente");
+                    redirectAttributes.addFlashAttribute("usuario", usuario);
+                } return "redirect:/usuario/nuevo";
+
+            } else if (usuario.getIdusuario() == 0) {
+                redirectAttributes.addFlashAttribute("msg", "Usuario creado exitosamente");
+            } else {
+                redirectAttributes.addFlashAttribute("msg", "Usuario actualizado exitosamente");
+            }
+        }
+        usuario.setContrasena(encriptar(usuario.getContrasena()));
         usuarioRepository.save(usuario);
-        return "redirect: /usuario";
+        return "redirect:/usuario/lista";
     }
+
     @GetMapping("/editar")
-    public String editarUsuario(@ModelAttribute("usuario") Usuario usuario, Model model, @RequestParam("id") int id, RedirectAttributes redirectAttributes){
+    public String editarUsuario(@ModelAttribute("usuario") Usuario usuario, Model model, @RequestParam("id") int id, RedirectAttributes redirectAttributes) {
+        model.addAttribute("listaroles", rolRepository.findAll());
+        model.addAttribute("listasedes", sedeRepository.findAll());
         Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
-        if(optionalUsuario.isPresent()){
+        if (optionalUsuario.isPresent()) {
             usuario = optionalUsuario.get();
             model.addAttribute("usuario", usuario);
             return "Usuario/form";
         } else {
-            return"redirect: /usuario/lista";
+            return "redirect: /usuario/lista";
         }
     }
 
+    public String encriptar(String pww) {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        pww = bCryptPasswordEncoder.encode(pww);
+        return pww;
+    }
 
 
 }
--- src/main/java/com/example/demo/Entity/Sede.java
+++ src/main/java/com/example/demo/Entity/Sede.java
@@ -3,10 +3,11 @@ package com.example.demo.Entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "sede")
-public class Sede {
+public class Sede implements Serializable {
     @Id
     private int idrol;
     private String sede;
