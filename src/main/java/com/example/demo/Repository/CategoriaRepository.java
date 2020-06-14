5c25a392f781ce7351a89a433fb5e690d062e42b
Eric Benites <eric.benites@pucp.edu.pe>
2020-06-14 14:08:05 -0500

    validaciones de categoria

--- src/main/java/com/example/demo/Repository/CategoriaRepository.java
+++ src/main/java/com/example/demo/Repository/CategoriaRepository.java
@@ -5,12 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
 
     @Query(value="SELECT nombrecategoria from categoria where nombrecategoria= ?1",nativeQuery=true)
      String verificarNombre(String nombreCategoria);
 
+    @Query(value = "SELECT * FROM categoria where idcategoria not in (Select c.idcategoria from categoria c where c.idcategoria=?1);"
+            , nativeQuery = true)
+    List<Categoria> mio(int id);
 
 
 }
