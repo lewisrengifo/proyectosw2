44047a6fd64d2cfa1290efbf37d35a49af13af36
Josue Romero <jcromero@pucp.edu.pe>
2020-06-18 23:27:39 -0500

    avance flujo inventario

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -32,6 +32,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         //http.authorizeRequests().antMatchers("/categoria", "/categoria/**").hasAnyAuthority( "Gestor principal", "Gestor sede");
         http.authorizeRequests().antMatchers("/producto", "/producto/**", "/comunidad", "/comunidad/**").hasAnyAuthority("Gestor principal", "Gestor sede");
         http.authorizeRequests().antMatchers("/artesano", "/artesano/**").hasAnyAuthority("Gestor principal", "Gestor sede");
+        http.authorizeRequests().antMatchers("/inventarioPrincipal", "/inventarioPrincipal/**").hasAnyAuthority("Gestor principal", "Gestor sede");
+
         http.authorizeRequests().antMatchers("/login", "/login/**").anonymous();
         http.authorizeRequests().anyRequest().permitAll();
 
