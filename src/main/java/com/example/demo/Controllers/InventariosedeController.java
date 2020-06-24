3e47e651ca146d4a26711c91ae70de9e177c1e53
Gabrielcv98 <gabrialbert98@gmail.com>
2020-06-23 23:06:54 -0500

    lista no terminada de inventariosede

--- src/main/java/com/example/demo/Controllers/InventariosedeController.java
+++ src/main/java/com/example/demo/Controllers/InventariosedeController.java
@@ -1,16 +1,25 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Entity.Artesano;
+import com.example.demo.Entity.Inventarioproducto;
 import com.example.demo.Entity.Inventariosede;
 import com.example.demo.Entity.Sede;
+import com.example.demo.Repository.InventarioSedeRepository;
 import com.example.demo.Repository.InventarioproductoRepository;
 import com.example.demo.Repository.SedeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/inventarioSede")
@@ -20,6 +29,8 @@ public class InventariosedeController {
     InventarioproductoRepository inventarioproductoRepository;
     @Autowired
     SedeRepository sedeRepository;
+    @Autowired
+    InventarioSedeRepository inventarioSedeRepository;
 
     @GetMapping("/asignarStock")
     public String asignarStock(Model model, @ModelAttribute("sede") Sede sede,
@@ -36,5 +47,17 @@ public class InventariosedeController {
 
         return "";
     }
+
+    @GetMapping(value = {"", "/lista"})
+    public String listaInventarioSede(Model model) {
+
+
+        model.addAttribute("listaInventarioSede",inventarioSedeRepository.findAll());
+
+
+        return "/inventario/inventariosede";
+    }
+
+
 
 }
