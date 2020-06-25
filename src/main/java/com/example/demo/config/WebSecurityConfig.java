788369c46eead57cb06d3424c20fc43d3ad28635
Josue Romero <jcromero@pucp.edu.pe>
2020-06-25 01:54:02 -0500

    flujos parte 2

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -30,8 +30,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests().antMatchers( "/sede", "/sede/**").hasAnyAuthority("Administrador", "Gestor principal");
         http.authorizeRequests().antMatchers( "/usuario", "/usuario/**").hasAuthority("Administrador");
-        //http.authorizeRequests().antMatchers( "/sede", "/sede/**").hasAuthority("Gestor sede");
-        //http.authorizeRequests().antMatchers("/categoria", "/categoria/**").hasAnyAuthority( "Gestor principal", "Gestor de sede");
+        http.authorizeRequests().antMatchers( "/ConsignacionVenta", "/ConsignacionVenta/**").hasAuthority("Gestor principal");
+        http.authorizeRequests().antMatchers("/categoria", "/categoria/**").hasAnyAuthority( "Gestor principal", "Gestor de sede");
         http.authorizeRequests().antMatchers("/producto", "/producto/**", "/comunidad", "/comunidad/**").hasAnyAuthority("Gestor principal", "Gestor sede");
         http.authorizeRequests().antMatchers("/artesano", "/artesano/**").hasAnyAuthority("Gestor principal", "Gestor sede");
         http.authorizeRequests().antMatchers("/login", "/login/**").permitAll();
