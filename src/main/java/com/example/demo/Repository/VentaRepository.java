383df8956f05c5c2bafbe0655ceba37f3e014364
Josue Romero <jcromero@pucp.edu.pe>
2020-06-29 02:32:15 -0500

    avance casi total de venta

--- src/main/java/com/example/demo/Repository/VentaRepository.java
+++ src/main/java/com/example/demo/Repository/VentaRepository.java
@@ -1,7 +1,15 @@
 package com.example.demo.Repository;
 
+import com.example.demo.Entity.Usuario;
 import com.example.demo.Entity.Ventas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 public interface VentaRepository extends JpaRepository<Ventas,Integer> {
+
+    @Query(value = "SELECT * FROM ventas where sede_idrol=?1", nativeQuery = true)
+    List<Ventas> listaVentasPorSede(int id);
 }
