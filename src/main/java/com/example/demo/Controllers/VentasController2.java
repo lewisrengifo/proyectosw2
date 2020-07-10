42ca596239c299ddd6e5cc89cb4689a07977e497
Eric Benites <eric.benites@pucp.edu.pe>
2020-07-10 03:46:35 -0500

    reportes casi terminado

--- src/main/java/com/example/demo/Controllers/VentasController2.java
+++ src/main/java/com/example/demo/Controllers/VentasController2.java
@@ -3,9 +3,7 @@ package com.example.demo.Controllers;
 import com.example.demo.Dto.ReporteMensualoAnualMosqoyDto;
 import com.example.demo.Entity.Comunidad;
 import com.example.demo.Entity.Sede;
-import com.example.demo.Repository.ComunidadRepository;
-import com.example.demo.Repository.SedeRepository;
-import com.example.demo.Repository.VentaRepository1;
+import com.example.demo.Repository.*;
 import com.example.demo.service.ServiceExcel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
@@ -145,6 +143,7 @@ public class VentasController2 {
         return ResponseEntity.ok().headers(headers).body(new InputStreamResource(stream));
     }
 
+    //--------------REPORTES POR SEDE--------------------------------------------
     @Autowired
     SedeRepository sedeRepository;
 
@@ -155,11 +154,129 @@ public class VentasController2 {
         return "Reportes/sede";
     }
 
-    @GetMapping("/articulos")
-    public String paginaReportesArticulo(){
-        return "Reportes/articulos";
+    @PostMapping("/ano/sede")
+    public ResponseEntity<InputStreamResource> exportDataAnualSede(@RequestParam("ano")String ano, Sede sede) throws Exception{
+
+        List<ReporteMensualoAnualMosqoyDto> lista= ventaRepository1.reporteMensualAnualSede(ano, sede.getIdsede());
+        ByteArrayInputStream stream = serviceExcel.exportarData(ano,lista,ano);//cambiar 2variable ano por un string que sea igual a lo correspondiente
+        HttpHeaders headers = new HttpHeaders();
+        String archivo = "Reporte anual del año"+ " "+ ano +" de la sede" + " " + sede.getNombre(); //titulo del excel, no del sheet
+        headers.add("Content-Disposition","attachment; filename="+ archivo +".xls");
+        return ResponseEntity.ok().headers(headers).body(new InputStreamResource(stream));
+    }
+    @PostMapping("/anomes/sede")
+    public ResponseEntity<InputStreamResource> exportDataAnualyMensualSede(@RequestParam("anomes")String anomes,@RequestParam("mes")String mes, Sede sede) throws Exception{
+        String mes1="";
+        switch (mes){
+            case "Enero":
+                mes1="01";
+                break;
+
+            case "Febrero":
+                mes1="02";
+                break;
+
+            case "Marzo":
+                mes1="03";
+                break;
+
+            case "Abril":
+                mes1="04";
+                break;
+
+            case "Mayo":
+                mes="05";
+                break;
+
+            case "Junio":
+                mes1="06";
+                break;
+
+            case "Julio":
+                mes1="07";
+                break;
+
+            case "Agosto":
+                mes1="08";
+                break;
+
+            case "Septiembre":
+                mes1="09";
+                break;
+
+            case "Octubre":
+                mes1="10";
+                break;
+
+            case "Noviembre":
+                mes1="11";
+                break;
+
+            case "Diciembre":
+                mes1="12";
+                break;
+            default: mes1="#";
+        }
+        String dato=anomes+"-"+mes1;
+        List<ReporteMensualoAnualMosqoyDto> lista= ventaRepository1.reporteMensualAnualSede(dato, sede.getIdsede());
+        String aux="Mes "+mes;
+        ByteArrayInputStream stream = serviceExcel.exportarData(aux,lista,anomes);
+        HttpHeaders headers = new HttpHeaders();
+        String archivo = "Reporte mensual de la sede" + " " + sede.getNombre(); //titulo del excel
+        headers.add("Content-Disposition","attachment; filename="+ archivo +".xls");
+        return ResponseEntity.ok().headers(headers).body(new InputStreamResource(stream));
+    }
+    @PostMapping("/trimestre/sede")
+    public ResponseEntity<InputStreamResource> exportDataAnualTrimestralSede(@RequestParam("trimestre")String trimestre,@RequestParam("anotri")String anotri, Sede sede) throws Exception{
+        String mes1="";
+        String mes2="";
+        String mes3="";
+        switch (trimestre){
+            case "Primero":
+                mes1="01";
+                mes2="02";
+                mes3="03";
+                break;
+
+            case "Segundo":
+                mes1="04";
+                mes2="05";
+                mes3="06";
+                break;
+
+            case "Tercero":
+                mes1="07";
+                mes2="08";
+                mes3="09";
+                break;
+
+            case "Cuarto":
+                mes1="10";
+                mes2="11";
+                mes3="12";
+                break;
+            default: mes1="#";
+        }
+        List<ReporteMensualoAnualMosqoyDto> lista= ventaRepository1.reporteTrimestralAnualMosqoy(mes1,mes2,mes3,anotri);
+        String aux="Trimestre "+ trimestre ;
+        ByteArrayInputStream stream = serviceExcel.exportarData(aux,lista,anotri);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition","attachment; filename=Ventas.xls");
+        return ResponseEntity.ok().headers(headers).body(new InputStreamResource(stream));
     }
 
+    //---------REPORTES POR ARTICULOS
+
+    @Autowired
+    ProductoRepository productoRepository;
+    @GetMapping("/articulos")
+    public String paginaReportesArticulo(Model model){
+        model.addAttribute("listaProducto", productoRepository.findAll());
+        return "Reportes/producto";
+    }
+
+
+    //REPORTES POR COMUNIDAD
     @Autowired
     ComunidadRepository comunidadRepository;
     @GetMapping("/comunidad")
@@ -168,9 +285,122 @@ public class VentasController2 {
         model.addAttribute("listaComunidad", listaComunidad);
         return "Reportes/comunidad";
     }
+    @PostMapping("/ano/comunidad")
+    public ResponseEntity<InputStreamResource> exportDataAnualComunidad(@RequestParam("ano")String ano, Comunidad comunidad) throws Exception{
+        List<ReporteMensualoAnualMosqoyDto> lista= ventaRepository1.reporteComunidad(ano, comunidad.getIdcomunidad());
+        ByteArrayInputStream stream = serviceExcel.exportarData(ano,lista,ano);//cambiar 2variable ano por un string que sea igual a lo correspondiente
+        HttpHeaders headers = new HttpHeaders();
+        String archivo = "Reporte anual del año " + ano +" de la comunidad" + " " + comunidad.getNombrecomunidad(); //titulo del excel, no del sheet
+        headers.add("Content-Disposition","attachment; filename="+ archivo +".xls");
+        return ResponseEntity.ok().headers(headers).body(new InputStreamResource(stream));
+    }
+    @PostMapping("/anomes/comunidad")
+    public ResponseEntity<InputStreamResource> exportDataAnualyMensualComunidad(@RequestParam("anomes")String anomes,@RequestParam("mes")String mes, Sede sede) throws Exception{
+        String mes1="";
+        switch (mes){
+            case "Enero":
+                mes1="01";
+                break;
 
+            case "Febrero":
+                mes1="02";
+                break;
+
+            case "Marzo":
+                mes1="03";
+                break;
+
+            case "Abril":
+                mes1="04";
+                break;
+
+            case "Mayo":
+                mes="05";
+                break;
+
+            case "Junio":
+                mes1="06";
+                break;
+
+            case "Julio":
+                mes1="07";
+                break;
+
+            case "Agosto":
+                mes1="08";
+                break;
+
+            case "Septiembre":
+                mes1="09";
+                break;
+
+            case "Octubre":
+                mes1="10";
+                break;
+
+            case "Noviembre":
+                mes1="11";
+                break;
+
+            case "Diciembre":
+                mes1="12";
+                break;
+            default: mes1="#";
+        }
+        String dato=anomes+"-"+mes1;
+        List<ReporteMensualoAnualMosqoyDto> lista= ventaRepository1.reporteMensualAnualSede(dato, sede.getIdsede());
+        String aux="Mes "+mes;
+        ByteArrayInputStream stream = serviceExcel.exportarData(aux,lista,anomes);
+        HttpHeaders headers = new HttpHeaders();
+        String archivo = "Reporte mensual de la comunidad" + " " + sede.getNombre(); //titulo del excel
+        headers.add("Content-Disposition","attachment; filename="+ archivo +".xls");
+        return ResponseEntity.ok().headers(headers).body(new InputStreamResource(stream));
+    }
+    @PostMapping("/trimestre/comunidad")
+    public ResponseEntity<InputStreamResource> exportDataAnualTrimestralComunidad(@RequestParam("trimestre")String trimestre,@RequestParam("anotri")String anotri, Sede sede) throws Exception{
+        String mes1="";
+        String mes2="";
+        String mes3="";
+        switch (trimestre){
+            case "Primero":
+                mes1="01";
+                mes2="02";
+                mes3="03";
+                break;
+
+            case "Segundo":
+                mes1="04";
+                mes2="05";
+                mes3="06";
+                break;
+
+            case "Tercero":
+                mes1="07";
+                mes2="08";
+                mes3="09";
+                break;
+
+            case "Cuarto":
+                mes1="10";
+                mes2="11";
+                mes3="12";
+                break;
+            default: mes1="#";
+        }
+        List<ReporteMensualoAnualMosqoyDto> lista= ventaRepository1.reporteTrimestralAnualMosqoy(mes1,mes2,mes3,anotri);
+        String aux="Trimestre "+ trimestre ;
+        ByteArrayInputStream stream = serviceExcel.exportarData(aux,lista,anotri);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition","attachment; filename=Ventas.xls");
+        return ResponseEntity.ok().headers(headers).body(new InputStreamResource(stream));
+    }
+
+    //REPORTES DE CLIENTE
+    @Autowired
+    VentasRepository ventasRepository;
     @GetMapping("/cliente")
-    public String paginaReportesCliente(){
+    public String paginaReportesCliente(Model model){
+        model.addAttribute("listaCliente", ventasRepository.findAll());
         return "Reportes/cliente";
     }
 
--- src/main/java/com/example/demo/Repository/VentaRepository1.java
+++ src/main/java/com/example/demo/Repository/VentaRepository1.java
@@ -47,4 +47,34 @@ public interface VentaRepository1 extends JpaRepository<Ventas,Integer> {
     List<ReporteMensualoAnualMosqoyDto> reporteTrimestralAnualMosqoy(@Param("mes1") String mes1,@Param("mes2") String mes2, @Param("mes3")String mes3, @Param("anotri")String anotri);
 
 
+    //sede anual y mensual
+    @Query(value = "SELECT  v.fechaventa as fechadeventa, v.tipodocumento as tipodedocumento, v.numerodocumento as documento, v.rucdni as rucodni,\n" +
+            " v.nombrecomprador as cliente, v.cantidad , p.codigoproducto as codigoproducto, p.nombreproducto as nombredeproducto,\n" +
+            " invpr.color as color , v.metodopago as metododepago\n" +
+            "  FROM proyectobasesw2.ventas v\n" +
+            "  inner join inventariosede invse on invse.idiventariosede = v.iventariosede_idiventariosede\n" +
+            "  inner join inventarioproducto invpr on invpr.idinventario = invse.inventarioproducto_idinventario\n" +
+            "  inner join producto p on p.idproducto = invpr.producto_idproducto\n" +
+            "  where fechaventa like %?1% and v.sede_idsede = ?2 ;", nativeQuery = true)
+    List<ReporteMensualoAnualMosqoyDto> reporteMensualAnualSede(String fechaventa, Integer sede);
+
+    //mensual/anual para comunidad
+    @Query(value = "SELECT v.nombrecomprador as cliente, v.numerodocumento as documento, v.lugarventa as lugar, v.tipodocumento as tipodocumento, v.fechaventa as fechaventa  FROM ventas v inner join inventariosede invs on invs.idiventariosede = v.iventariosede_idiventariosede\n" +
+            "inner join inventarioproducto invp on invp.idinventario = invs.inventarioproducto_idinventario\n" +
+            "inner join consignacionyventa consve on consve.idconsignacion = invp.consignacionyventa_idconsignacion\n" +
+            "inner join artesano art on art.idartesano = consve.artesano_idartesano\n" +
+            "inner join comunidad com on com.idcomunidad = art.comunidad_idcomunidad\n" +
+            "where v.fechaventa like %?1% and com.idcomunidad = ?2;", nativeQuery = true)
+    List<ReporteMensualoAnualMosqoyDto> reporteComunidad(String fechaventa, Integer comunidad);
+
+    //mensual/anual para cliente
+    @Query(value = "SELECT  v.fechaventa as fechadeventa, v.tipodocumento as tipodedocumento, v.numerodocumento as documento, v.rucdni as rucodni,\n" +
+            " v.nombrecomprador as cliente, v.cantidad , p.codigoproducto as codigoproducto, p.nombreproducto as nombredeproducto,\n" +
+            " invpr.color as color , v.metodopago as metododepago\n" +
+            "  FROM proyectobasesw2.ventas v\n" +
+            "  inner join inventariosede invse on invse.idiventariosede = v.iventariosede_idiventariosede\n" +
+            "  inner join inventarioproducto invpr on invpr.idinventario = invse.inventarioproducto_idinventario\n" +
+            "  inner join producto p on p.idproducto = invpr.producto_idproducto\n" +
+            "  where fechaventa like '%2020%' and v.nombrecomprador like 'Josue Romero';", nativeQuery = true)
+    List<ReporteMensualoAnualMosqoyDto> reporteCliente(String fechaventa, Integer comunidad);
 }
